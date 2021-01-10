5e803852085901c5c2f9c9a0fd07734a7d2637dd
niu2x <niu2x@icloud.com>
2021-01-10 22:35:45 +0800

    personal test

--- public/view/index.php
+++ public/view/index.php
@@ -14,7 +14,9 @@ require('model/app.php')
   </head>
   <body>
 
-    
+    <ul>
+      <li><a href="modules/Personal/index.html">木婉清</a></li>
+    </ul>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
   </body>
