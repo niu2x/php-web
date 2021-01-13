061a172ae275385d4ec94ed39e1d806bbded75f1
niu2x <niu2x@icloud.com>
2021-01-14 02:39:27 +0800

    display client ip

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
 <?php
   require('view/index.php');
 ?>
+
+<p><?php echo "REMOTE_ADDR: {$_SERVER['REMOTE_ADDR']}\n"; ?></p>
+<p><?php echo "HTTP_X_FORWARDED_FOR: {$_SERVER['HTTP_X_FORWARDED_FOR']}\n"; ?></p>
